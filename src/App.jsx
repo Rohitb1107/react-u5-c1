c9b99f2e2940a88a9a49fa7ccc8a67ad9459f47c
Rohit <stylishrb711@gmail.com>
2022-04-11 12:01:00 +0530

    style part

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,81 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Score, setScore] = useState(76);
+  const [Wicket, setWicket] = useState(2);
+  const [Over, setOver] = useState(50);
+
+  const changeScore = (value) => {
+    if (Score < 0) {
+      return;
+    }
+    setScore(Score + value);
+  };
+
+  const changeWicket = (value) => {
+    if (Wicket < 0 || Wicket >= 12) {
+      return;
+    }
+    setWicket(Wicket + value);
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
+    <>
+      <div className="App">
+        <h3>India:</h3>
+        <div className="banner">
+          <div>
+            Score: <h1 className="scoreCount">{Score}</h1>
+          </div>
+          <div>
+            Wicket: <h1 className="wicketCount">{Wicket}</h1>
+          </div>
+
+          <div>
+            Over:{" "}
+            <h1 className="overCount">
+              {
+                // Show Over here in the format: "over.ball" eg: 4.5 means 4th over and 5th ball
+                // if 1 more ball is thrown then over is now 5.0
+                // you have to write logic to form this string from current ball number.
+                Over
+              }
+            </h1>
+          </div>
+        </div>
+
+        <div className="addScore">
+          <h2>Add Score</h2>
+          {/* these buttons should add the respective amount in the score */}
+          <button className="addScore1" onClick={() => changeScore(1)}>
+            Add 1
           </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+          <button className="addScore4" onClick={() => changeScore(4)}>
+            Add 4
+          </button>
+          <button className="addScore6" onClick={() => changeScore(6)}>
+            Add 6
+          </button>
+        </div>
+
+        <div className="addWicket" onClick={() => changeWicket(1)}>
+          Add Wicket
+          {/* Increase the count of wicket */}
+          <button>Add 1 wicket</button>
+        </div>
+
+        <div className="addBall">
+          Add ball
+          {/* Increase the total number of balls thrown here. */}
+          <button>Add 1</button>
+        </div>
+
+        {/* If score reaches greater than 100, show text "India Won" without quotes in h1 tag with class name 'status' */}
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
